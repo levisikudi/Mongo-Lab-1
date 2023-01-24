a5a9f2b8826923a03edd5bbac8a0545aa5a762ca
Levis Sikudi <levisikudi@gmail.com>
2023-01-24 12:43:23 -0500

    css update and display fruit functionality completed

--- public/display_food/index.js
+++ public/display_food/index.js
@@ -1,7 +1,20 @@
+let container = document.getElementById('container')
+
 const getData = async () =>{
     let data = await fetch('/get_food_data');
     data.json().then((parsedData) =>{
         console.log(parsedData);
+
+        parsedData.forEach(element => {
+            let pTag = document.createElement('p');
+            pTag.textContent = element.name
+            container.appendChild(pTag)
+            if (element.readyToEat){
+                pTag.style.backgroundColor = "green"
+            }else{
+                pTag.style.backgroundColor = "red"
+            }
+        });
     })
 }
 getData()
